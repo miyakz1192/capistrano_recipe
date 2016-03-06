f86d5c5449adc8faf624a9f93819c1b4d614c9b3
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2016-03-06 17:50:23 +0900

    updated

--- neutron/config/deploy.rb
+++ neutron/config/deploy.rb
@@ -23,7 +23,22 @@ task :deploy do
 #    neutrons.current_ver(:bigger_than_equals => "0.9") # NOT ERR OK 
     
 #    neutrons.current_ver(:equals => "1.0") # ERR OK
-#    neutrons.depends_on(neutrons, :later_ver => "2.0")
+    
+# =================================
+    
+#    neutrons.depends_on(neutrons, :lower_than => "2.1") # NOT ERROR OK
+#    neutrons.depends_on(neutrons, :lower_than => "2.0") # ERROR OK
+#    neutrons.depends_on(neutrons, :lower_than => "1.9") # ERROR OK
+#    neutrons.depends_on(neutrons, :lower_than => "1.0") # ERROR OK 
+#    neutrons.depends_on(neutrons, :lower_than => "0.9") # ERROR OK 
+   
+#    neutrons.depends_on(neutrons, :bigger_than_equals => "2.1") # ERROR OK
+#    neutrons.depends_on(neutrons, :bigger_than_equals => "2.0") # ERROR OK
+#    neutrons.depends_on(neutrons, :bigger_than_equals => "1.9") # ERROR OK
+#    neutrons.depends_on(neutrons, :bigger_than_equals => "1.0") # NOT ERR OK 
+#    neutrons.depends_on(neutrons, :bigger_than_equals => "0.9") # NOT ERR OK 
+   
+#    neutrons.depends_on(neutrons, :equals => "1.0") # ERR OK
   end
 end
 logger.info "[END] DEPLOY"
